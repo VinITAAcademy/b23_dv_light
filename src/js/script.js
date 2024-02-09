4cfaf5f68460f5d7a3e64338315c3c59d4e0e0a9
ArturBas4477 <basartur020@gmail.com>
2024-02-09 15:36:36 -0800

    bloc_footer_br2

--- src/js/script.js
+++ src/js/script.js
@@ -52,9 +52,12 @@ window.addEventListener("DOMContentLoaded", function () {
         formValidate(form, buttons);
       });
     });
-    $("#phone-participant, #phone-partner, #phone-mentor").on("mouseenter", function () {
-      $(this).attr("placeholder", "");
-    });
+    $("#phone-participant, #phone-partner, #phone-mentor").on(
+      "mouseenter",
+      function () {
+        $(this).attr("placeholder", "");
+      }
+    );
 
     form.addEventListener(eventType, function (event) {
       event.preventDefault();
